dd3d5b57eccb9f8e4dd074459d710be8d01bd298
Shijie Li <sli474@bloomberg.net>
2017-06-09 16:55:26 -0400

    changed prompt line

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -58,5 +58,20 @@ bindkey -M viins 'jk' vi-cmd-mode
 # Use incremental search
 bindkey "^R" history-incremental-search-backward
 
+
+# setup prompt line
+git_branch_info() {
+    git_prompt_text="$(git symbolic-ref HEAD 2>/dev/null | cut -d'/' -f3)" || ""
+    if [[ -n $git_prompt_text ]]; then
+        echo " <$git_prompt_text>"
+    else
+        echo ""
+    fi
+}
+PROMPT='%{$fg[green]%}%n@%m %{$fg[cyan]%}%c%{$fg[yellow]%}$(git_branch_info)%{$reset_color%} $ '
+
+# Custom prompt
+#source ~/.zsh/prompt.zsh
+
 # setup fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
